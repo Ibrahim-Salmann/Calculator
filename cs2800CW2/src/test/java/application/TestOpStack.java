bd13a2b0e1c14385da6c77f0f76071bd5e6ce1c4
Mohammad Salman <wlis107@live.rhul.ac.uk>
2023-11-30 00:09:18 +0000

    Created a JUnit test file called TestOpStack that will test the push, pop, and top methods that takes Symbol values as entries.

---
+++ cs2800CW2/src/test/java/application/TestOpStack.java
@@ -0,0 +1,13 @@
+package application;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+class TestOpStack {
+
+  @Test
+  void test() {
+    fail("Not yet implemented");
+  }
+
+}
