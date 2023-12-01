8c01c98d1a50c6fd92dd711e399c0c5742de5365
Mohammad Salman <wlis107@live.rhul.ac.uk>
2023-12-01 21:45:55 +0000

    Now that the stack codes have been completed, I move to programming the two different notations of mathematical expressions. I first strat with Reverse Polish (postfix) which the operators come after their operands. In simpler terms, you write the numbers first and then the operation. I created a new branch called Feature-RevPolishCalc and a new JUnit test case file TestRevPolishCalc.java.

---
+++ cs2800CW2/src/test/java/application/TestRevPolishCalc.java
@@ -0,0 +1,13 @@
+package application;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+class TestRevPolishCalc {
+
+  @Test
+  void test() {
+    fail("Not yet implemented");
+  }
+
+}
