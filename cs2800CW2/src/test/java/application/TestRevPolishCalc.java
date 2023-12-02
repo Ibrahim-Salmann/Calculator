80ac03567d987dcd7076f6388a463f5fe3a312dd
Mohammad Salman <wlis107@live.rhul.ac.uk>
2023-12-02 01:08:33 +0000

    Test 2: testAddiction() added is designed to assess the addition functionality of the Reverse Polish Notation (RPN) evaluation implemented in the `RevPolishCalc.java` file. The test covers a range of inputs to validate the accuracy of the addition operation. The evaluation method, which includes the necessary addition logic, was previously added and now successfully handles diverse input scenarios, resulting in a passing test.

--- cs2800CW2/src/test/java/application/TestRevPolishCalc.java
+++ cs2800CW2/src/test/java/application/TestRevPolishCalc.java
@@ -24,4 +24,13 @@ class TestRevPolishCalc {
     assertThrows(InvalidExpressionException.class, () -> RPC.evaluate("6 6 x"));
   }
 
+  @Test
+  // Test 2;
+  void testAddiction() throws BadTypeException, InvalidExpressionException {
+    assertEquals(RPC.evaluate("9 9 +"), 18f);
+    assertEquals(RPC.evaluate("15 28 +"), 43f);
+    assertEquals(RPC.evaluate("12 16 + 5 + 14 + 1"), 47f);
+    assertEquals(RPC.evaluate("70 100 + 66 + 10 + 12 + 55 +"), 313f);
+  }
+
 }
