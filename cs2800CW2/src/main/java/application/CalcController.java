c8b283211e1eadd33cc39abb01fabfac133304b1
Mohammad Salman <wlis107@live.rhul.ac.uk>
2023-12-04 22:49:37 +0000

    CalcController modified for managing interactions between CalcModel and View
    Created the CalcController class,
    which serves as the intermediary between the calculator model (CalcModel) responsible for actual evaluations and the user interface view (ViewInterface).
    The controller handles user-triggered calculations and changes in calculation types.
    
    - Implemented methods for handling calculation and calculation type change.
    - Integrated with CalcModel and ViewInterface.
    - Set up observers for calculation and type change events in the view.
    - Started the user interface upon construction.
    
    This enhancement improves the organization and functionality of the calculator application by introducing a dedicated controller to manage user interactions.

--- cs2800CW2/src/main/java/application/CalcController.java
+++ cs2800CW2/src/main/java/application/CalcController.java
@@ -3,14 +3,61 @@ package application;
 /**
  * The controller that sits between the calculator model that does actual evaluation and the view
  * that is the part the user interfaces with.
+ * 
+ * @author Ibraheem
  */
 public class CalcController {
   private CalcModel myModel;
   private ViewInterface myView;
 
-  private void handleCalculation() {}
+  /**
+   * Handles the calculation process triggered by the user.
+   */
+  private void handleCalculation() {
+    try {
+      // Retrieve the expression from the view
+      String expression = myView.getExpression();
 
-  private void handleTypeChange() {}
+      // Evaluate the expression using the model
+      float result = myModel.evaluate(expression, true); // Assuming infix notation
 
-  CalcController(CalcModel model, ViewInterface view) {}
+      // Display the result in the view
+      myView.setAnswer(Float.toString(result));
+    } catch (InvalidExpressionException e) {
+      // Handle invalid expression exception, e.g., display an error message in the view
+      myView.setAnswer("Error: " + e.getMessage());
+    }
+  }
+
+  /**
+   * Handles the change in calculation type (infix or reverse polish).
+   * 
+   * @param type The calculation type to set in the model.
+   */
+  private void handleTypeChange(OpType type) {
+    // Update the calculation type in the model
+    // Assuming OpType is an enumeration representing infix or reverse polish
+    // Modify the CalcModel and OpType classes accordingly
+    // myModel.setCalculationType(type);
+  }
+
+  /**
+   * Constructs a new CalcController with the specified model and view.
+   * 
+   * @param model The calculator model.
+   * @param view The user interface view.
+   */
+  CalcController(CalcModel model, ViewInterface view) {
+
+
+    this.myModel = model;
+    this.myView = view;
+
+    // Add observers to the view
+    myView.addCalculateObserver(this::handleCalculation);
+    myView.addTypeObserver(this::handleTypeChange);
+
+    // Start the user interface
+    myView.startView();
+  }
 }
